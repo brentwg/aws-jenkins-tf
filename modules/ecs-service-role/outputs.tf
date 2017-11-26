b1595c5c226c79a4de9341bda9f2b74fa54a2c8d
Brent Gawryluik <brent.gawryluik@scalar.ca>
2017-11-26 00:26:43 -0800

    Removed ECS service role module

--- modules/ecs-service-role/outputs.tf
+++
@@ -1,6 +0,0 @@
-# --------------
-# Module Outputs
-# --------------
-output "ecs_service_role_arn" {
-  value = "${aws_iam_role.ecs_service_role.arn}"
-}
--- modules/ecs-service-role/variables.tf
+++
@@ -1,5 +0,0 @@
-# -------------
-# Module Inputs
-# -------------
-variable "customer_name" {}
-variable "environment" {}
