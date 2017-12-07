77e78fee76ee18e6077f67964c12687f2bd2c686
Brent Gawryluik <brent.gawryluik@scalar.ca>
2017-12-07 13:58:31 -0800

    Modified ELB to also accept HTTP

--- modules/jenkins-elb/main.tf
+++ modules/jenkins-elb/main.tf
@@ -6,6 +6,7 @@ resource "aws_elb" "this" {
   security_groups = ["${var.elb_security_groups}"]
   internal        = "${var.elb_internal}"
 
+
   listener {
     instance_port      = "${var.int_web_port}"
     instance_protocol  = "HTTP"
@@ -44,5 +45,15 @@ resource "aws_lb_cookie_stickiness_policy" "this" {
   name                     = "${var.customer_name}-${var.environment}-elb-stickiness-policy"
   load_balancer            = "${aws_elb.this.id}"
   lb_port                  = "${var.ext_web_port}"
+  cookie_expiration_period = "${var.elb_cookie_expiration_period}"
+}
+
+# ---------------------------
+# ELB Stickiness Policy - SSL
+# ---------------------------
+resource "aws_lb_cookie_stickiness_policy" "this" {
+  name                     = "${var.customer_name}-${var.environment}-elb-stickiness-policy-ssl"
+  load_balancer            = "${aws_elb.this.id}"
+  lb_port                  = "${var.ext_ssl_port}"
   cookie_expiration_period = "${var.elb_cookie_expiration_period}"
 }
